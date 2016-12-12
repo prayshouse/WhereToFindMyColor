25f251d511b72199d4993a6354112b95f166947f
shouse <3140103730@zju.edu.cn>
2016-12-12 18:23:26 +0800

    add move and jump function

---
+++ Assets/Script/BallMove.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    public Vector3 mPosition;
+
+    private GameObject mBall;
+    private Vector3 mDirection;
+    private GameObject mCamera;
+    private bool mGrounded;
+    //private float mGravity;
+
+    private Rigidbody mRigidbody;
+
+    private void Awake()
+    {
+        moveSpeed = 0.4f;
+        jumpSpeed = 8.0f;
+        mGrounded = true;
+        //mGravity = 20.0f;
+    }
+
+    void Start ()
+    {
+        mDirection = new Vector3(1.0f, 0.0f, 0.0f);
+        mBall = gameObject;
+        mPosition = mBall.transform.position;
+        mCamera = GameObject.Find("Main Camera");
+        mRigidbody = mBall.GetComponent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        mDirection = mCamera.GetComponent<FirstView>().direction;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        if (Input.GetKey(KeyCode.W))
+        {
+            mPosition = transform.position + mDirection * moveSpeed;
+            mBall.transform.position = mPosition;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            mPosition = transform.position - mDirection * moveSpeed;
+            mBall.transform.position = mPosition;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            mPosition = transform.position + new Vector3(-mDirection.z, 0.0f, mDirection.x) * moveSpeed;
+            mBall.transform.position = mPosition;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mPosition = transform.position + new Vector3(mDirection.z, 0.0f, -mDirection.x) * moveSpeed;
+            mBall.transform.position = mPosition;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+
+	}
+
+    void jump()
+    {
+        if (mGrounded == true)
+        {
+            print("jump mGround:true -> false");
+            mRigidbody.AddForce(Vector3.up * jumpSpeed);
+            mRigidbody.velocity = new Vector3(0.0f, 15.0f, 0.0f);
+            mGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("fall mGround:false -> ture");
+        mGrounded = true;
+    }
+}
---
+++ Assets/Script/FirstView.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstView : MonoBehaviour {
+
+    //方向灵敏度  
+    public float sensitivityX = 10.0f;
+    public float sensitivityY = 10.0f;
+
+    //上下最大视角(Y视角)  
+    public float minimumY = -60.0f;
+    public float maximumY = 60.0f;
+
+    float rotationX = 0.0f;
+    float rotationY = 0F;
+    public Vector3 direction;
+
+    private BallMove mBallMove;
+
+    private void Awake()
+    {
+        direction = new Vector3(1.0f, 0.0f, 0.0f);
+        Cursor.visible = false;
+    }
+
+    void Start ()
+    {
+        GameObject ball = GameObject.Find("Ball");
+        mBallMove = ball.GetComponent<BallMove>();
+
+        Rigidbody r = GetComponent<Rigidbody>();
+        if (r) r.freezeRotation = true;
+        
+    }
+
+    void Update()
+    {
+
+        //根据鼠标移动的快慢(增量), 获得相机左右旋转的角度(处理X)  
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        //根据鼠标移动的快慢(增量), 获得相机上下旋转的角度(处理Y)  
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //角度限制. rotationY小于min,返回min. 大于max,返回max. 否则返回value   
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        //总体设置一下相机角度  
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        
+        direction.z = Mathf.Cos(rotationX * Mathf.Deg2Rad);
+        direction.x = Mathf.Sin(rotationX * Mathf.Deg2Rad);
+    }
+}
